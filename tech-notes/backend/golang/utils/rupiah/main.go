99314ce2fa97b14423bbf8247e2c68d77ab18145
rizkyzhang <rizkyzhangdev@gmail.com>
2022-12-20 21:40:16 +0700

    feat(golang/utils): add rupiah formatter

---
+++ tech-notes/backend/golang/utils/rupiah/main.go
@@ -0,0 +1,19 @@
+package rupiah
+
+import (
+	"fmt"
+
+	"github.com/bojanz/currency"
+)
+
+func FormatRupiah(value uint64) (string, error) {
+	amount, err := currency.NewAmount(fmt.Sprint(value), "IDR")
+	if err != nil {
+		return "", err
+	}
+	locale := currency.NewLocale("id")
+	formatter := currency.NewFormatter(locale)
+	formatter.MaxDigits = 0
+
+	return formatter.Format(amount), nil
+}
